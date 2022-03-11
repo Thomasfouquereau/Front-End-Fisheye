ba841d6708e307c289f0d62d6cea8286d4a59351
Fouquereau <thomas.fouquereau@gmail.com>
2022-03-11 10:55:31 +0100

    media add

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,3 +1,10 @@
+import { photographerFactory } from "../factories/photographer.js";
+import { mediaFactory } from "../factories/photographer.js";
+import { closeModal, displayModal } from "../utils/contactForm.js";
+
+document.getElementById("contactButton").addEventListener("click", displayModal)
+document.getElementById("closeModal").addEventListener("click", closeModal)
+
 async function getPhotographerInfo(userId) {
     try {
         const response = await fetch('/data/photographers.json');
@@ -11,53 +18,52 @@ async function getPhotographerInfo(userId) {
     }
 }
 
-//async function getmediaInfo() {
-//    try {
-//        const response = await fetch('/data/photographers.json');
-//        const data = await response.json();
-//        return ({
-//            photographers: [...data.media]
-//        })
-//    } catch {//
-//        console.log('error')
-//    }
-//}
-//image non fonctionnel voir pq????
-//et like avec incrémentation +1 et voir le svg like 
+async function getMediaInfo(userId) {
+    try {
+        const response = await fetch('/data/photographers.json');
+        const data = await response.json();
+        const filterMedia = data.media.filter(function (media) {
+            return media.photographerId === userId;
+        });
+        return filterMedia[0]
+    } catch {
+        console.log('error')
+    }
+}
+
 
 function displayData(photographer) {
     const photographersInfo = document.querySelector(".info");
     const photographersImg = document.querySelector(".photograph-img");
-    const mediaContent = document.querySelector(".content");
-     //photographers.forEach((photographer) => {
     const photographerModel = photographerFactory(photographer);
     const userInfoDOM = photographerModel.getUserInfoDOM();
     const userImgDOM = photographerModel.getUserImgDOM();
-    const usermediaDOM = photographerModel.getUserMediaDOM();
-    mediaContent.appendChild(usermediaDOM);
     photographersInfo.appendChild(userInfoDOM);
     photographersImg.appendChild(userImgDOM);
-    // });
 }
-//couper la fonction en deux ..media ..photographer
+
+function displayMedia(media) {
+    const mediaModel = mediaFactory(media);
+    const mediaContent = document.querySelector(".content");
+    const usermediaDOM = mediaModel.getUserMediaDOM();
+    mediaContent.appendChild(usermediaDOM);
+}
 
 async function initInfo() {
     try {
-        const url = new URL(window.location.href);//en voire plus sur url search prarams
-        const params = new URLSearchParams(url.search);//--
-        const photographerId = Number(params.get("id"));//--
+        const url = new URL(window.location.href);
+        const params = new URLSearchParams(url.search);
+        const photographerId = Number(params.get("id"));
         const selectedPhotographer = await getPhotographerInfo(photographerId);
         displayData(selectedPhotographer);
-        console.log(url.searchParams.get("id"));
+        const selectedMedia = await getMediaInfo(photographerId);//forEach
+        const firstName = selectedPhotographer.name.split(" ")[0];
+        selectedMedia.src =  `assets/images/${firstName}/${selectedMedia.image || selectedMedia.video}`;
+        displayMedia(selectedMedia); 
     } catch (error) {
         alert(error.message);
     }
-
-    // const { media } = await getmediaInfo();
-    // displayData(media)
 }
 
+
 initInfo();
-
-
-import { photographerFactory } from "../factories/photographer.js";
